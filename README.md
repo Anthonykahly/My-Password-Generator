33cad5e780d54bcac51398e091cf7ea8c17747f8
A K <akahly@gmail.com>
2023-07-20 16:20:37 -0400

    fix

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # My-Password-Generator
-My Password Generator
+
+## Description
+This password generator uses a combination of user inputs to form randomized passwords to protect user information and accounts.
+
+## Installation
+
+N/A
+
+## Usage
+
+Upon opening the page the site will prompt users to input infromation they require for their password such as:
+- Character lenth (8-128)
+- Capital and Undercase letters
+- Numeric Integers
+- Special characters
+The site will then create a randomly generated password for maximum security.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+![](assets/frontpage.png) 
---
+++ script.js
@@ -0,0 +1,133 @@
+// Assignment Code
+
+
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+
+
+function writePassword() {
+  var password = RPG();
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+
+
+}
+
+
+// Add event listener to generate button
+
+
+generateBtn.addEventListener("click", writePassword);
+
+
+// Array of possible lower case characters
+var LCCA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+// Array of possible upper case characters
+var UCCA = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+// Array of possible special case characters
+var SCA = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+
+// Array of possible numeric characters
+var NCA = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+// Returns a random integer
+function RandomGenerator(array) {
+  var ArrayIndex = Math.floor(Math.random() * array.length);
+  var RandomArray = array[ArrayIndex];
+  return RandomArray;
+}
+
+
+// Creates the password based on user input
+
+
+function RPG() {
+  // Vars created with placeholders
+
+
+  var concatArray = [];
+  var mixedCharactersArray = [];
+
+
+  // Variable that will return an integer as long as it is within 8 to 128 characters
+
+
+  var passwordLength = parseInt
+    (prompt("Please choose a number between 8-128 characters for your password."));
+  if (passwordLength < 8) {
+    alert("Your password must be 8 characters or longer!")
+  }
+  if (passwordLength > 128) {
+    alert("Your password must be 128 characters or less!")
+  }
+
+
+  // Variable for lower case
+  var selectedLower = confirm("Would you like characters?");
+
+
+  // Variable for upper case
+  var selectedUpper = confirm("Would you like characters?");
+
+
+  // Variable for special characters
+  var selectedSpecial = confirm("Would you like special characters?");
+
+
+  // Variable for numeric characters
+  var selectedNumeric = confirm("Would you like characters?");
+
+
+  // Function for input validation
+
+
+  if (!selectedSpecial &&
+    !selectedNumeric &&
+    !selectedLower &&
+    !selectedUpper) {
+    alert("You must choose at least one type of character!");
+    return;
+  }
+
+
+  // Adds every character types-array depending on what the user selects and pushes the randomly generated response
+
+
+  if (selectedSpecial) {
+    mixedCharactersArray = mixedCharactersArray.concat(SCA);
+  }
+  if (selectedNumeric) {
+    mixedCharactersArray = mixedCharactersArray.concat(NCA);
+  }
+  if (selectedLower) {
+    mixedCharactersArray = mixedCharactersArray.concat(LCCA);
+  }
+  if (selectedUpper) {
+    mixedCharactersArray = mixedCharactersArray.concat(UCCA);
+  }
+
+
+  // Iterating over passwordLength and pushing concatArray character
+
+
+  for (var i = 0; i < passwordLength; i++) {
+    var character = RandomGenerator(mixedCharactersArray);
+    concatArray.push(character);
+  }
+
+
+  // returns and joins the concatinated array
+
+
+  return concatArray.join('');
+}
